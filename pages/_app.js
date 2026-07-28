e12e23e77d3b2a122b9a42f23cdead2ba24be7c4
MagicNDTMan <mike@ndt.llc>
2026-07-27 23:31:31 -0400

    Add Google Fonts to global head

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,15 @@ import '../styles/globals.css';
 import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@400;600;700&family=Barlow:wght@400;600&display=swap" rel="stylesheet" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
